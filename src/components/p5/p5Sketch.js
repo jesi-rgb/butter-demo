effb256c3df8eb4b2a25e9d407c90ca03a98513d
Jesús Enrique Rascón <blograso@gmail.com>
2023-03-06 11:32:36 +0100

    video is working!

--- src/components/p5/p5Sketch.js
+++ src/components/p5/p5Sketch.js
@@ -1,34 +1,32 @@
 import dynamic from "next/dynamic";
-import { useState } from "react";
 
 const Sketch = dynamic(() => import("react-p5").then((mod) => mod.default), {
   ssr: false,
 });
 
 export default function P5Sketch() {
-  const [maskShader, setShader] = useState(null);
+  let maskShader;
+  let bg, fg;
 
-  //   let preload = (p5) => {
-  //     setShader(
-  //       p5.loadShader("shaders/maskShader.vert", "shaders/maskShader.frag")
-  //     );
-  //   };
-
+  let preload = (p5) => {
+    maskShader = p5.loadShader(
+      "shaders/maskShader.vert",
+      "shaders/maskShader.frag"
+    );
+  };
   let setup = (p5, parentRef) => {
-    p5.createCanvas(600, 600, p5.WEBGL).parent(parentRef);
+    p5.createCanvas(1000, 600, p5.WEBGL).parent(parentRef);
 
     console.log("mierdon");
 
-    let bg, fg;
-
     const bgPromise = new Promise((resolve) => {
-      bg = p5.createVideo("videos/video.webm", resolve);
+      bg = p5.createVideo("videos/josh.mp4", resolve);
       bg.volume(0);
       bg.hide();
     });
 
     const fgPromise = new Promise((resolve) => {
-      fg = p5.createVideo("videos/mask_1200x1200.mp4", resolve);
+      fg = p5.createVideo("videos/josh-mask.mp4", resolve);
       fg.volume(0);
       fg.hide();
     });
@@ -42,11 +40,11 @@ export default function P5Sketch() {
 
   let draw = (p5) => {
     p5.clear();
-    // p5.shader(maskShader);
-    // maskShader.setUniform("bg", bg);
-    // maskShader.setUniform("fg", fg);
-    p5.noStroke();
+    p5.shader(maskShader);
+    maskShader.setUniform("bg", bg);
+    maskShader.setUniform("fg", fg);
+    p5.fill(3);
     p5.plane(p5.width, p5.height);
   };
-  return <Sketch setup={setup} draw={draw} />;
+  return <Sketch preload={preload} setup={setup} draw={draw} />;
 }
