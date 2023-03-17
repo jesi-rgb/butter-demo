a189d4dc1ec3e322d56da3f50162c7b0f63d589a
Jesús Enrique Rascón <blograso@gmail.com>
2023-03-17 13:51:00 +0100

    trying to deal with drop

--- src/components/p5/VideoSandwichStacked.jsx
+++ src/components/p5/VideoSandwichStacked.jsx
@@ -7,18 +7,22 @@ const Sketch = dynamic(() => import("react-p5").then((mod) => mod.default), {
 
 export default function VideoSandwhichStacked({ children, stackedVideoPath }) {
   let stackedVideo;
-  let [enableVideo, setEnableVideo] = useState(true);
+  let [enableVideo, setEnableVideo] = useState(false);
 
-  let setup = (p5, parentRef) => {
-    let canvas = p5.createCanvas(1280, 720).parent(parentRef);
+  let setupBackground = (p5, parentRef) => {
+    console.log("setup run");
+
+    let cnv = p5.createCanvas(1280, 720).parent(parentRef);
     stackedVideo = p5.createVideo(stackedVideoPath);
     stackedVideo.volume(0);
     stackedVideo.hide();
     stackedVideo.loop();
+
+    cnv.drop(() => p5.print("hola"));
   };
 
   let drawBackGround = (p5) => {
-    p5.clear();
+    p5.noStroke();
     p5.image(stackedVideo, 0, 0, 1280, 720, 0, 0, 1280, 720);
   };
 
@@ -33,11 +37,13 @@ export default function VideoSandwhichStacked({ children, stackedVideoPath }) {
   };
 
   let setupForeground = (p5, parentRef) => {
-    p5.createCanvas(1280, 720, p5.WEBGL).parent(parentRef);
+    let cnv = p5.createCanvas(1280, 720, p5.WEBGL).parent(parentRef);
 
     // load and set the shader
     p5.shader(maskShader);
     maskShader.setUniform("video", stackedVideo);
+
+    cnv.drop(() => console.log("hola"));
   };
 
   let drawForeground = (p5) => {
@@ -49,21 +55,15 @@ export default function VideoSandwhichStacked({ children, stackedVideoPath }) {
     <>
       <div className="m-20 flex justify-center">
         {/*background video. this video is rendered as is, untouched*/}
-        <div
-          className="-z-10 absolute mx-auto pointer-events-none"
-          style={{ visibility: enableVideo ? "visible" : "hidden" }}
-        >
-          <Sketch setup={setup} draw={drawBackGround} />
+        <div className="-z-10 absolute mx-auto pointer-events-none">
+          <Sketch setup={setupBackground} draw={drawBackGround} />
         </div>
 
         {/*children of the component that lie in between the two sketches*/}
         <div className="z-0 absolute">{children}</div>
 
         {/*foreground video. this video is masked from the runway mask*/}
-        <div
-          className="z-50 absolute pointer-events-none mx-auto"
-          style={{ visibility: enableVideo ? "visible" : "hidden" }}
-        >
+        <div className="z-50 absolute pointer-events-none mx-auto">
           <Sketch
             preload={preload}
             setup={setupForeground}
