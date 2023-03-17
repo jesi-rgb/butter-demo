bf04eaa18077670552cd5e36cf479fd8cc43bb91
Jesús Enrique Rascón <blograso@gmail.com>
2023-03-17 11:36:10 +0100

    changed controls yay

--- src/components/3D/Scene.jsx
+++ src/components/3D/Scene.jsx
@@ -1,12 +1,14 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useThree } from "@react-three/fiber";
 import { OrbitControls, Preload, Select } from "@react-three/drei";
 import { Suspense, useRef, useState } from "react";
 import { Panel } from "./MultiLeva";
+import { MOUSE } from "three";
 
 export default function Scene({ children, fitScreen = false }) {
   // Everything defined in here will persist between route changes, only children are swapped
   let ref = useRef();
   const [selected, setSelected] = useState([]);
+
   return (
     <>
       <div
@@ -34,6 +36,10 @@ export default function Scene({ children, fitScreen = false }) {
 
             <Preload all />
             <OrbitControls
+              mouseButtons={{
+                LEFT: MOUSE.RIGHT,
+                RIGHT: MOUSE.LEFT,
+              }}
               enableZoom={true}
               dampingFactor={0.3}
               enableRotate={true}
